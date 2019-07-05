48c4f4cf512ff4dfda0095eeed7fe1397d6eda8a
Loui Sakaki <e1352207@outlook.jp>
2019-07-05 21:41:39 +0900

    updated

--- pyokaka/okaka/convert.py
+++ pyokaka/okaka/convert.py
@@ -1,8 +1,16 @@
+from ..util.priority_dict import PriorityDict
+
 import json
 from pathlib import Path
 
 
 _dir = Path(__file__).parent
+
+def _make_sokuon_tpls(items):
+    return [
+        ('{}{}'.format(key[0], key), 'っ{}'.format(value))
+        for key, value in items
+    ]
 
 def _load_transtable():
     with (_dir / '../transtable.json').open() as fin:
@@ -11,23 +19,24 @@ def _load_transtable():
     convert_tpls = [
         (key, value) for value, keys in data.items() for key in keys
     ]
-    convert_tpls += [
-        ('{}{}'.format(key[0], key), 'っ{}'.format(value)) 
-        for key, value in convert_tpls if key not in 'aiueon'
-    ]
+    convert_tpls += _make_sokuon_tpls(
+        filter(lambda kv: kv[-1] not in 'あいうえおん', convert_tpls)
+    )
     convert_tpls += [('\'', ''), ('-', 'ー')]
 
-    convert_tpls.sort(key=lambda e: len(e[0]), reverse=True)
-    
-    return convert_tpls
+    pd = PriorityDict(lambda k: 100-len(k))
+    pd.update(convert_tpls)
+    return pd
 
 
-_convert_tpls = _load_transtable()
+#
+#
+_convert_dct = _load_transtable()
 def convert(roman_sentence):
     roman_sentence = roman_sentence.lower()
 
     def step():
-        for key, value in _convert_tpls:
+        for key, value in _convert_dct.items():
             if roman_sentence.startswith(key):
                 return key, value
         
@@ -43,3 +52,17 @@ def convert(roman_sentence):
 
     return ''.join(ret)
         
+
+def update_convert_dct(dct, *, top_priority=False):
+    items = tuple(dct.items())
+    items = {
+        **dct, **dict(_make_sokuon_tpls(items))
+    }
+
+    print(items)
+
+    if top_priority:
+        _convert_dct.update(items, priority_func=lambda _: 0)
+    else:
+        _convert_dct.update(items)
+
